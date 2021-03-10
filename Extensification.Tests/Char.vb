def1f545a561b13d873a7f14cbf0114590c704af
EoflaOE <eoflaoevicecity@gmail.com>
2021-03-10 16:02:28 +0200

    Added Char extensions

---
+++ Extensification.Tests/Char.vb
@@ -0,0 +1,54 @@
+﻿
+'    Extensification  Copyright (C) 2020-2021  EoflaOE
+'
+'    This file is part of Extensification
+'
+'    Extensification is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+'
+'    Extensification is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+Imports Extensification.CharExts
+
+<TestClass>
+Public Class CharTests
+
+    ''' <summary>
+    ''' Tests character incrementation
+    ''' </summary>
+    <TestMethod>
+    Public Sub TestIncrement()
+        Dim Target As Char = "A"
+        Target = Target.Increment(4)
+        Assert.AreEqual("E"c, Target)
+    End Sub
+
+    ''' <summary>
+    ''' Tests character decrementation
+    ''' </summary>
+    <TestMethod>
+    Public Sub TestDecrement()
+        Dim Target As Char = "E"
+        Target = Target.Decrement(4)
+        Assert.AreEqual("A"c, Target)
+    End Sub
+
+    ''' <summary>
+    ''' Tests character ASCII code fetching
+    ''' </summary>
+    <TestMethod>
+    Public Sub TestGetAsciiCode()
+        Dim Target As Char = "E"
+        Assert.AreEqual(69, Target.GetAsciiCode)
+    End Sub
+
+End Class
---
+++ Extensification/Char.vb
@@ -0,0 +1,59 @@
+﻿
+'    Extensification  Copyright (C) 2020-2021  EoflaOE
+'
+'    This file is part of Extensification
+'
+'    Extensification is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+'
+'    Extensification is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Imports System.Runtime.CompilerServices
+
+Namespace CharExts
+    Public Module Extensions
+
+        ''' <summary>
+        ''' Increments the character
+        ''' </summary>
+        ''' <param name="Character">Character</param>
+        ''' <param name="IncrementThreshold">How many times to increment</param>
+        ''' <returns>Incremented character</returns>
+        <Extension>
+        Public Function Increment(ByVal Character As Char, ByVal IncrementThreshold As Integer) As Char
+            If IncrementThreshold < 0 Then Throw New InvalidOperationException("Threshold is negative. Use Decrement().")
+            Return ChrW(AscW(Character) + IncrementThreshold)
+        End Function
+
+        ''' <summary>
+        ''' Decrements the character
+        ''' </summary>
+        ''' <param name="Character">Character</param>
+        ''' <param name="DecrementThreshold">How many times to decrement</param>
+        ''' <returns>Decremented character</returns>
+        <Extension>
+        Public Function Decrement(ByVal Character As Char, ByVal DecrementThreshold As Integer) As Char
+            If DecrementThreshold < 0 Then Throw New InvalidOperationException("Threshold is negative. Use Increment().")
+            Return ChrW(AscW(Character) - DecrementThreshold)
+        End Function
+
+        ''' <summary>
+        ''' Gets an ASCII code of a character
+        ''' </summary>
+        ''' <param name="Character">Character</param>
+        ''' <returns>ASCII code of a character</returns>
+        <Extension>
+        Public Function GetAsciiCode(ByVal Character As Char) As Integer
+            Return AscW(Character)
+        End Function
+
+    End Module
+End Namespace
