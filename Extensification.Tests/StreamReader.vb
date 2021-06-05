f76135b511f14147e1b0fd903409d4c656203f89
EoflaOE <eoflaoevicecity@gmail.com>
2021-06-05 13:55:44 +0300

    Added support for StreamReader

---
+++ Extensification.Tests/StreamReader.vb
@@ -0,0 +1,39 @@
+﻿
+'    Extensification  Copyright (C) 2020-2021  EoflaOE
+'
+'    This file is part of Extensification
+'
+'    Extensification is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+'
+'    Extensification is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Imports System.IO
+Imports Extensification.StreamReaderExts
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class StreamReaderTests
+
+    ''' <summary>
+    ''' Tests reading a line from the stream reader with the newline characters
+    ''' </summary>
+    <TestMethod>
+    Public Sub TestReadLineWithNewLine()
+        Dim TargetText As String = "Hello! This is Extensification." + Environment.NewLine +
+                                   "You've reached the second line!"
+        Dim TextStream As New MemoryStream(Text.Encoding.Default.GetBytes(TargetText))
+        Dim TextStreamReader As New StreamReader(TextStream)
+        Dim ParsedText As String = TextStreamReader.ReadLineWithNewLine
+        Assert.IsTrue(ParsedText.EndsWith(Environment.NewLine))
+    End Sub
+
+End Class
---
+++ Extensification/StreamReader.vb
@@ -0,0 +1,49 @@
+﻿
+'    Extensification  Copyright (C) 2020-2021  EoflaOE
+'
+'    This file is part of Extensification
+'
+'    Extensification is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+'
+'    Extensification is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Imports System.IO
+Imports System.Runtime.CompilerServices
+Imports System.Text
+
+Namespace StreamReaderExts
+    Public Module Extensions
+
+        ''' <summary>
+        ''' Reads a line from the stream with the newline characters
+        ''' </summary>
+        ''' <param name="reader">The stream reader</param>
+        <Extension>
+        Public Function ReadLineWithNewLine(ByVal reader As StreamReader) As String
+            'Define a new string builder
+            Dim strBuilder As New StringBuilder()
+
+            'Read files as character numbers and convert them to string
+            While Not reader.EndOfStream
+                Dim readFile As Integer = reader.Read
+                strBuilder.Append(ChrW(readFile))
+                If readFile = 10 Then
+                    Exit While
+                End If
+            End While
+
+            'Return the final result
+            Return strBuilder.ToString()
+        End Function
+
+    End Module
+End Namespace
