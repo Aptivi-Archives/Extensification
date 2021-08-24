368edb188c55c34fa5ca109a905a2af89fa7d664
EoflaOE <eoflaoevicecity@gmail.com>
2021-08-24 15:06:36 +0300

    Added stream writer test

---
+++ Extensification/Streams/StreamWriter/Writing.vb
@@ -0,0 +1,40 @@
+﻿
+'    Extensification  Copyright (C) 2020-2021  EoflaOE
+'
+'    This file is part of Extensification
+'
+'    Extensification is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+'
+'    Extensification is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Imports System.IO
+Imports System.Runtime.CompilerServices
+Imports System.Text
+
+Namespace StreamWriterExts
+    Public Module Writing
+
+        ''' <summary>
+        ''' Writes all the lines to the stream
+        ''' </summary>
+        ''' <param name="writer">The stream writer</param>
+        <Extension>
+        Public Sub WriteLines(ByRef writer As StreamWriter, ByVal Lines() As String)
+            For Each Line As String In Lines
+                writer.WriteLine(Line)
+                If Not writer.AutoFlush Then writer.Flush()
+            Next
+            If writer.BaseStream.CanSeek Then writer.BaseStream.Seek(0, SeekOrigin.Begin)
+        End Sub
+
+    End Module
+End Namespace
