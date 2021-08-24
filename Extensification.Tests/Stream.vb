b8060b943f17988444b92b7a074f74b7ba4c10ef
EoflaOE <eoflaoevicecity@gmail.com>
2021-08-24 18:16:34 +0300

    Added TrySeek

---
+++ Extensification.Tests/Stream.vb
@@ -0,0 +1,40 @@
+﻿
+'    Extensification  Copyright (C) 2020-2021  EoflaOE
+'
+'    This file is part of Extensification
+'
+'    Extensification is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+'
+'    Extensification is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Imports System.IO
+Imports Extensification.StreamExts
+Imports Microsoft.VisualStudio.TestTools.UnitTesting
+
+<TestClass>
+Public Class StreamTests
+
+#Region "Manipulation"
+    ''' <summary>
+    ''' Tests trying to seek in a stream
+    ''' </summary>
+    <TestMethod>
+    Public Sub TestTrySeek()
+        Dim TargetText As String = "Hello! This is Extensification." + Environment.NewLine +
+                                   "You've reached the second line!"
+        Dim TextStream As New MemoryStream(Text.Encoding.Default.GetBytes(TargetText))
+        Assert.IsTrue(TextStream.TrySeek(4, SeekOrigin.Begin))
+        Assert.IsTrue(TextStream.Position = 4)
+    End Sub
+#End Region
+
+End Class
---
+++ Extensification/Streams/Stream/Manipulation.vb
@@ -0,0 +1,45 @@
+﻿
+'    Extensification  Copyright (C) 2020-2021  EoflaOE
+'
+'    This file is part of Extensification
+'
+'    Extensification is free software: you can redistribute it and/or modify
+'    it under the terms of the GNU General Public License as published by
+'    the Free Software Foundation, either version 3 of the License, or
+'    (at your option) any later version.
+'
+'    Extensification is distributed in the hope that it will be useful,
+'    but WITHOUT ANY WARRANTY; without even the implied warranty of
+'    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+'    GNU General Public License for more details.
+'
+'    You should have received a copy of the GNU General Public License
+'    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+Imports System.IO
+Imports System.Runtime.CompilerServices
+
+Namespace StreamExts
+    Public Module Manipulation
+
+        ''' <summary>
+        ''' Tries seeking the stream
+        ''' </summary>
+        ''' <returns>True if successful; False if unsuccessful</returns>
+        <Extension>
+        Public Function TrySeek(ByVal TargetStream As Stream, Offset As Long, Origin As SeekOrigin) As Boolean
+            Dim Success As Boolean = True
+
+            'Try to seek
+            Try
+                TargetStream.Seek(Offset, Origin)
+            Catch ex As Exception
+                Success = False
+            End Try
+
+            'Return the result
+            Return Success
+        End Function
+
+    End Module
+End Namespace
